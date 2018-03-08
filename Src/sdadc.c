660e4dfe2e473d8fdb8cc08a0a54820e9a8c80fe
REV\ReV <caginbaris@gmail.com>
2018-03-08 22:36:30 +0300

    first commit ea_project

---
+++ Src/sdadc.c
@@ -0,0 +1,242 @@
+/**
+  ******************************************************************************
+  * File Name          : SDADC.c
+  * Description        : This file provides code for the configuration
+  *                      of the SDADC instances.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2018 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "sdadc.h"
+
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+SDADC_HandleTypeDef hsdadc1;
+SDADC_HandleTypeDef hsdadc2;
+SDADC_HandleTypeDef hsdadc3;
+
+/* SDADC1 init function */
+void MX_SDADC1_Init(void)
+{
+
+    /**Configure the SDADC low power mode, fast conversion mode,
+    slow clock mode and SDADC1 reference voltage 
+    */
+  hsdadc1.Instance = SDADC1;
+  hsdadc1.Init.IdleLowPowerMode = SDADC_LOWPOWER_NONE;
+  hsdadc1.Init.FastConversionMode = SDADC_FAST_CONV_DISABLE;
+  hsdadc1.Init.SlowClockMode = SDADC_SLOW_CLOCK_DISABLE;
+  hsdadc1.Init.ReferenceVoltage = SDADC_VREF_EXT;
+  if (HAL_SDADC_Init(&hsdadc1) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+/* SDADC2 init function */
+void MX_SDADC2_Init(void)
+{
+
+    /**Configure the SDADC low power mode, fast conversion mode,
+    slow clock mode and SDADC1 reference voltage 
+    */
+  hsdadc2.Instance = SDADC2;
+  hsdadc2.Init.IdleLowPowerMode = SDADC_LOWPOWER_NONE;
+  hsdadc2.Init.FastConversionMode = SDADC_FAST_CONV_DISABLE;
+  hsdadc2.Init.SlowClockMode = SDADC_SLOW_CLOCK_DISABLE;
+  hsdadc2.Init.ReferenceVoltage = SDADC_VREF_EXT;
+  if (HAL_SDADC_Init(&hsdadc2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+/* SDADC3 init function */
+void MX_SDADC3_Init(void)
+{
+
+    /**Configure the SDADC low power mode, fast conversion mode,
+    slow clock mode and SDADC1 reference voltage 
+    */
+  hsdadc3.Instance = SDADC3;
+  hsdadc3.Init.IdleLowPowerMode = SDADC_LOWPOWER_NONE;
+  hsdadc3.Init.FastConversionMode = SDADC_FAST_CONV_DISABLE;
+  hsdadc3.Init.SlowClockMode = SDADC_SLOW_CLOCK_DISABLE;
+  hsdadc3.Init.ReferenceVoltage = SDADC_VREF_EXT;
+  if (HAL_SDADC_Init(&hsdadc3) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+void HAL_SDADC_MspInit(SDADC_HandleTypeDef* sdadcHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  if(sdadcHandle->Instance==SDADC1)
+  {
+  /* USER CODE BEGIN SDADC1_MspInit 0 */
+
+  /* USER CODE END SDADC1_MspInit 0 */
+    /* SDADC1 clock enable */
+    __HAL_RCC_SDADC1_CLK_ENABLE();
+  
+    /**SDADC1 GPIO Configuration    
+    PB2     ------> SDADC1_AIN4P 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SDADC1_MspInit 1 */
+
+  /* USER CODE END SDADC1_MspInit 1 */
+  }
+  else if(sdadcHandle->Instance==SDADC2)
+  {
+  /* USER CODE BEGIN SDADC2_MspInit 0 */
+
+  /* USER CODE END SDADC2_MspInit 0 */
+    /* SDADC2 clock enable */
+    __HAL_RCC_SDADC2_CLK_ENABLE();
+  
+    /**SDADC2 GPIO Configuration    
+    PE9     ------> SDADC2_AIN7P 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SDADC2_MspInit 1 */
+
+  /* USER CODE END SDADC2_MspInit 1 */
+  }
+  else if(sdadcHandle->Instance==SDADC3)
+  {
+  /* USER CODE BEGIN SDADC3_MspInit 0 */
+
+  /* USER CODE END SDADC3_MspInit 0 */
+    /* SDADC3 clock enable */
+    __HAL_RCC_SDADC3_CLK_ENABLE();
+  
+    /**SDADC3 GPIO Configuration    
+    PB14     ------> SDADC3_AIN8P 
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_14;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN SDADC3_MspInit 1 */
+
+  /* USER CODE END SDADC3_MspInit 1 */
+  }
+}
+
+void HAL_SDADC_MspDeInit(SDADC_HandleTypeDef* sdadcHandle)
+{
+
+  if(sdadcHandle->Instance==SDADC1)
+  {
+  /* USER CODE BEGIN SDADC1_MspDeInit 0 */
+
+  /* USER CODE END SDADC1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDADC1_CLK_DISABLE();
+  
+    /**SDADC1 GPIO Configuration    
+    PB2     ------> SDADC1_AIN4P 
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_2);
+
+  /* USER CODE BEGIN SDADC1_MspDeInit 1 */
+
+  /* USER CODE END SDADC1_MspDeInit 1 */
+  }
+  else if(sdadcHandle->Instance==SDADC2)
+  {
+  /* USER CODE BEGIN SDADC2_MspDeInit 0 */
+
+  /* USER CODE END SDADC2_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDADC2_CLK_DISABLE();
+  
+    /**SDADC2 GPIO Configuration    
+    PE9     ------> SDADC2_AIN7P 
+    */
+    HAL_GPIO_DeInit(GPIOE, GPIO_PIN_9);
+
+  /* USER CODE BEGIN SDADC2_MspDeInit 1 */
+
+  /* USER CODE END SDADC2_MspDeInit 1 */
+  }
+  else if(sdadcHandle->Instance==SDADC3)
+  {
+  /* USER CODE BEGIN SDADC3_MspDeInit 0 */
+
+  /* USER CODE END SDADC3_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_SDADC3_CLK_DISABLE();
+  
+    /**SDADC3 GPIO Configuration    
+    PB14     ------> SDADC3_AIN8P 
+    */
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_14);
+
+  /* USER CODE BEGIN SDADC3_MspDeInit 1 */
+
+  /* USER CODE END SDADC3_MspDeInit 1 */
+  }
+} 
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
