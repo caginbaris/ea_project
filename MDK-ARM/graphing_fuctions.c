b063bd6e18cd17403a26f4a8d3814c70df54c761
caginbaris <31400655+caginbaris@users.noreply.github.com>
2018-05-26 15:54:55 +0300

    graphing part continued

--- MDK-ARM/graphing_fuctions.c
+++ MDK-ARM/graphing_fuctions.c
@@ -13,57 +13,103 @@ union LCD_map bitmap;
 
 //graphing lines
 
-void graphBaseLining(){
+//0,0 up left--64,128 down right
+
+//x position of hLine
+//line start vertical
+//line end   vertical
+
+void setbit(uint8_t x,uint8_t y){
+
+	uint8_t page;
+	uint8_t column;
+	uint8_t xBit;
+	uint8_t xData;
 	
-	uint8_t rowPosition;
-	uint8_t rowPage;
-	uint8_t rowData;
+	page=(uint8_t)(x/8);
+	xBit=x%8;
+	
+	switch(xBit){
+	
+		case 0: xData=bitmap.bit.b0;
+		case 1: xData=bitmap.bit.b1;
+		case 2: xData=bitmap.bit.b2;
+		case 3: xData=bitmap.bit.b3;
+		case 4: xData=bitmap.bit.b4;
+		case 5: xData=bitmap.bit.b5;
+		case 6: xData=bitmap.bit.b6;
+		case 7: xData=bitmap.bit.b7;
+	
+	}
+		
+	display_buffer[page][y]=xData;
+
+
+}
+void hline(uint8_t x, uint8_t lineStart,uint8_t lineEnd){
+	
+	uint8_t xPage;
+	uint8_t xBit;
+	uint8_t xData;
+	uint8_t i;
+	
+	xPage=(uint8_t)(x/8);
+	xBit=x%8;
+	
+	switch(xBit){
+	
+		case 0: xData=bitmap.bit.b0;
+		case 1: xData=bitmap.bit.b1;
+		case 2: xData=bitmap.bit.b2;
+		case 3: xData=bitmap.bit.b3;
+		case 4: xData=bitmap.bit.b4;
+		case 5: xData=bitmap.bit.b5;
+		case 6: xData=bitmap.bit.b6;
+		case 7: xData=bitmap.bit.b7;
+	
+	}
+	
+	
+	for(i=lineStart;i<lineEnd;i++){
+	
+	display_buffer[xPage][i]=xData;
+	
+	}
+	
+}
+
+
+void vline(uint8_t y, uint8_t lineStart,uint8_t lineEnd){
+
+	uint8_t startPage;
+	uint8_t startBit;
+	uint8_t startData;
+	uint8_t endPage;
+	uint8_t endBit;
 	uint8_t i;
 	
 	
-	//horizontal line start
+	startPage=(uint8_t)(lineStart/8);
+	startBit=lineStart%8;
 	
-	rowPage=(uint8_t)(hLineRow/8);
-	rowPosition=hLineRow%8;
+	switch(startBit){
 	
-	
-	switch(rowPosition){
-	
-		case 0: rowData=bitmap.bit.b0;
-		case 1: rowData=bitmap.bit.b1;
-		case 2: rowData=bitmap.bit.b2;
-		case 3: rowData=bitmap.bit.b3;
-		case 4: rowData=bitmap.bit.b4;
-		case 5: rowData=bitmap.bit.b5;
-		case 6: rowData=bitmap.bit.b6;
-		case 7: rowData=bitmap.bit.b7;
-	
-	}
-	
-	for(i=hLineRowStart;i<128;i++){
-	
-	display_buffer[rowPage][hLineRowStart]=rowData;
+		case 0: startData=0xFF; //cau row sequnce has to be checked
+		case 1: startData=0xFE;
+		case 2: startData=bitmap.bit.b2;
+		case 3: startData=bitmap.bit.b3;
+		case 4: startData=bitmap.bit.b4;
+		case 5: startData=bitmap.bit.b5;
+		case 6: startData=bitmap.bit.b6;
+		case 7: startData=bitmap.bit.b7;
 	
 	}
 	
 	
+	display_buffer[startPage][y]=startData;
 	
-	
-	/*
-	for(i=hLineRowStart;i<128;i++){
-	
-	display_buffer[rowPage][i]=rowData;
-	
-	}*/
-	
-	
-
-	
-	
-	
-	
-	
-	//horizontal_line
+	endPage=(uint8_t)(lineEnd/8);
+	endBit=lineEnd%8;
 	
 	
 	
@@ -71,9 +117,11 @@ void graphBaseLining(){
 	
 	
 	
+	for(i=startPage;i<endPage;i++){
 	
+	display_buffer[i][y]=0xFF;
 	
-	
+	}
 	
 	
 	
@@ -81,5 +129,20 @@ void graphBaseLining(){
 
 
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+void graphBaseLining(){
+	
 
 }
