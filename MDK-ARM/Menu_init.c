3ec1cd5bf3bbaeacdb31af9e440932750fb90179
Viva-PC\Viva <caginbaris@gmail.com>
2019-02-04 20:37:41 +0200

    save commit

--- MDK-ARM/Menu_init.c
+++ MDK-ARM/Menu_init.c
@@ -7,7 +7,7 @@
 
 
 
-enum menu_list current_menu=set_output;
+enum menu_list current_menu=Vpn_true;
 enum menu_list previous_menu=Vpp_true;
 
 enum main_menu_list    main_menu_entry=Active_Energy_main;
